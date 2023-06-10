18343a65205c9bded2018ac47fd583856a8db0f0
carsakiller <carsakiller@gmail.com>
2023-06-10 00:29:54 -0400

    fix: epoch is integer

--- library/types/general.lua
+++ library/types/general.lua
@@ -1,3 +1,3 @@
 ---@meta
 
----@alias epoch number A number of milliseconds since the [UNIX epoch](https://en.wikipedia.org/wiki/Unix_time). Useful for timestamping
+---@alias epoch integer A number of milliseconds since the [UNIX epoch](https://en.wikipedia.org/wiki/Unix_time). Useful for timestamping
